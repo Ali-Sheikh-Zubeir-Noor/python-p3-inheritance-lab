703608edf16c6c1726c3e7e111cc3246cc46acd2
Ali Sheikh Zubeir Noor <ali.noor1@student.moringaschool.com>
2025-05-19 23:39:20 +0300

    Adds changes

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,10 @@
 
 from user import User
 
-class Student(User):
-    
-    def learn(self):
-        pass
+class Student(User):       
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)  # Call parent constructor
+        self.knowledge = []  # Own attribute
+
+    def learn(self, string):
+        self.knowledge.append(string)
--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
 
 class User:
-    pass
+    def __init__(self, first_name, last_name):
+        first_name = first_name
+        last_name = last_name
+        
+    
